0e9e491a432204ad01b009125e53ea971a3214ed
juand <juandimu1309@hotmail.com>
2026-04-21 19:24:48 -0500

    remove: Documentation and build scripts - not needed for app

--- fix-textrun.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('create-doc.js', 'utf8');
-c = c.replace(/new TextRun\("([^"]*)"\)/g, 'new TextRun({ text: "$1" })');
-fs.writeFileSync('create-doc.js', c);
-console.log('Fix done');
